53a961bee1c7b9b210c0457c57d9acfa8d1306a6
Casper Schipper <casper.schipper@gmail.com>
2022-09-28 21:42:46 +0200

    yes this works, kind of

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Browser exposing (Document)
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Parser exposing ((|.), (|=), Parser, number)
 
 
 type Model
@@ -13,8 +14,75 @@ type Msg
     = NoOp
 
 
+type Sexpr
+    = Slist (List Sexpr)
+    | Value CValue
+
+
+type CValue
+    = CNumber Float
+    | CString String
+
+
+type CispWord
+    = Seq
+
+
+cispNumber : Parser CValue
+cispNumber =
+    Parser.number
+        { int = Just (\i -> i |> toFloat |> CNumber)
+        , hex = Nothing -- 0x001A is allowed
+        , octal = Nothing -- 0o0731 is not
+        , binary = Nothing -- 0b1101 is not
+        , float = Just CNumber
+        }
+
+
+value : Parser CValue
+value =
+    Parser.oneOf
+        [ cispNumber
+        , Parser.chompWhile Char.isAlpha |> Parser.getChompedString |> Parser.map CString
+        ]
+
+
+clist : Parser Sexpr
+clist =
+    Parser.sequence
+        { start = "("
+        , separator = " "
+        , end = ")"
+        , spaces = Parser.succeed ()
+        , item =
+            Parser.oneOf
+                [ Parser.lazy (\_ -> sexpr) -- hmm why does this one have to come first ?
+                , value |> Parser.map Value
+                ]
+        , trailing = Parser.Forbidden
+        }
+        |> Parser.map Slist
+
+
+sexpr : Parser Sexpr
+sexpr =
+    clist
+
+
+parse str =
+    Parser.run sexpr str
+
+
+input =
+    "(seq 1 (seq 4 5) 3)"
+
+
 init _ =
-    ( Model "(seq 1 2 3)", Cmd.none )
+    let
+        _ =
+            Debug.log input (Parser.run sexpr input)
+    in
+    ( Model input, Cmd.none )
 
 
 main : Platform.Program () Model Msg
@@ -29,7 +97,7 @@ main =
 
 viewChar : Char -> Html Msg
 viewChar c =
-    Html.span [] [Html.text (String.fromChar c)]
+    Html.span [] [ Html.text (String.fromChar c) ]
 
 
 showText : String -> Html Msg
@@ -38,7 +106,7 @@ showText str =
         chars =
             String.toList str
     in
-    Html.p [ Attr.style "font-family" "monospace" ] <| (List.map viewChar chars)
+    Html.p [ Attr.style "font-family" "monospace" ] <| List.map viewChar chars
 
 
 view : Model -> Document Msg
